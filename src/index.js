e805891cf70a85ac683454d585d9a7544fe8f6b9
pedropatriota <pedro.p.patriota@gmail.com>
2021-07-19 19:17:40 -0300

    project finished

--- src/index.js
+++ src/index.js
@@ -10,14 +10,12 @@ import {GlobalStyle} from './styles/globalStyle'
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <GlobalStyle/>
       <App />
+      <GlobalStyle/>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
--- src/redux/slices/postsSlice.js
+++ src/redux/slices/postsSlice.js
@@ -2,33 +2,32 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import api from '../../service/api';
 
 const initialState = {
-  posts:[],
-  loading: false
-}
+  posts:[], 
+  loading:false
+};
 
-export const fetchPosts = createAsyncThunk(() =>{
+export const fetchPosts = createAsyncThunk(
   'posts/fetch',
-  async()=>{
+  async () => {
     const response = await api.get('posts');    
     return response.data;
   }
-})
+);
 
 export const postsSlice = createSlice({
   name: 'posts',
   initialState,  
   reducers: {},
-
-  extraReducers: {
-    [fetchPosts.pending]: (state)=>{
-      state.loading= true
+  
+  extraReducers: {    
+    [fetchPosts.pending] : (state)=>{
+      state.loading = true
     },
-
-    [fetchPosts.fulfilled]: (state, action)=>{
-      state.posts =  action.payload;
-      state.loading= false
+     [fetchPosts.fulfilled] : (state, action)=>{
+      state.posts = action.payload
     }
   }
 })
 
-export default postsSlice.reducer
+
+export default postsSlice.reducer;
